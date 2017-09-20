062dbc3396da8dd16884de640284e11a221656cb
valttepe <Valtteri.Pesu@metropolia.fi>
2017-09-20 13:58:44 +0300

    ConstraintLayouts started

--- app/src/main/java/com/example/valtteri/journeytracker/RouteFragment.java
+++ app/src/main/java/com/example/valtteri/journeytracker/RouteFragment.java
@@ -1,16 +1,20 @@
 package com.example.valtteri.journeytracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Valtteri on 19.9.2017.
  */
 
 public class RouteFragment extends Fragment {
+
+    Button startbtn;
 
     public RouteFragment() {
         // Required empty public constructor
@@ -19,6 +23,14 @@ public class RouteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_route, container, false);
+        startbtn = (Button)v.findViewById(R.id.start_button);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent changetoTracking = new Intent(getActivity(), RouteTrackActivity.class);
+                startActivity(changetoTracking);
+            }
+        });
         return v;
     }
 }
