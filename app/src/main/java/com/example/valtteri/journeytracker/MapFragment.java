595873726b8ac3130fe84d5ba4bdddba2e71bfc0
odexi <otakar93@gmail.com>
2017-09-22 15:42:41 +0300

    Location stuff

--- app/src/main/java/com/example/valtteri/journeytracker/MapFragment.java
+++ app/src/main/java/com/example/valtteri/journeytracker/MapFragment.java
@@ -1,26 +1,296 @@
 package com.example.valtteri.journeytracker;
 
+import android.*;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Valtteri on 19.9.2017.
  */
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient gac;
+    Location loc;
+    FusedLocationProviderClient mFusedLocationClient;
+    LocationRequest mLocationRequest;
+    LocationCallback mLocationCallback;
+    private static final int REQUEST_CHECK_SETTINGS = 61124;
+
+    boolean mRequestingLocationUpdates;
+    double lati;
+    double longi;
+    MainActivity mainActivity;
+
+    protected void createLocationRequest() {
+        //LocationRequest
+        Log.d("location request", "Normally created");
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(3000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
     public MapFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+
+        org.osmdroid.tileprovider.constants
+                .OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
+
+        final MapView map = (MapView) v.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        final IMapController mapController = map.getController();
+        mapController.setZoom(18);
+
+
+        final Marker marker = new Marker(map);
+
+
+        gac = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(getActivity());
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(getActivity(), new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                Log.d("success", "yes?");
+            }
+        });
+
+        task.addOnFailureListener(getActivity(), new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Activity activity = getActivity();
+                if (activity != null && isAdded()) {
+
+                    Log.d("success", "nope? " + e);
+                    int statusCode = ((ApiException) e).getStatusCode();
+                    switch (statusCode) {
+                        case CommonStatusCodes.RESOLUTION_REQUIRED:
+                            // Location settings are not satisfied, but this can be fixed
+                            // by showing the user a dialog.
+                            try {
+                                // Show the dialog by calling startResolutionForResult(),
+                                // and check the result in onActivityResult().
+
+                                ResolvableApiException resolvable = (ResolvableApiException) e;
+                                resolvable.startResolutionForResult(activity,
+                                        REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException sendEx) {
+                                // Ignore the error.
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            // Location settings are not satisfied. However, we have no way
+                            // to fix the settings so we won't show the dialog.
+                            break;
+                    }
+                }
+            }
+        });
+
+
+        Log.d("location callback", "normally created");
+        mLocationCallback = new LocationCallback() {
+            public void onLocationResult(LocationResult locationResult) {
+                Log.d("Location result", locationResult.toString());
+                for (Location location : locationResult.getLocations()) {
+
+                    Double lat = location.getLatitude();
+                    Double lon = location.getLongitude();
+                    String la = lat.toString();
+                    String lo = lon.toString();
+                    /*mLatitude.setText(la);
+                    mLongitude.setText(lo);*/
+
+                    //MapView map = (MapView) findViewById(R.id.map);
+                    // IMapController mapController = map.getController();
+
+                    GeoPoint startPoint = new GeoPoint(lat, lon);
+
+                    marker.setPosition(startPoint);
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                    marker.setIcon(getResources().getDrawable(R.drawable.ic_my_location_black_24dp));
+                    map.getOverlays().add(marker);
+
+                    Log.d("LATI ON TÄMÄ", Double.toString(lat));
+                    Log.d("LONGI ON TÄMÄ", Double.toString(lon));
+                    mapController.setCenter(startPoint);
+
+                }
+            }
+        };
+
+
         return v;
+    }
+
+
+    public void onStart() {
+        gac.connect();
+        super.onStart();
+        createLocationRequest();
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d("resume", "test " + mRequestingLocationUpdates);
+        //if(mRequestingLocationUpdates) {
+        startLocationUpdates();
+        //}
+
+    }
+
+    public void onStop() {
+        super.onStop();
+        gac.disconnect();
+
+    }
+
+
+    protected void startLocationUpdates() {
+
+        try {
+
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback,
+                    null);
+            Log.d("loc update", "normally yes?");
+        } catch (Exception e) {
+            Log.d("loc update", "nope?" + e);
+
+        }
+
+    }
+
+
+
+
+
+
+    public void onConnected(Bundle connectionHint) {
+        Log.d("loc connected", "maybe?");
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        loc = LocationServices.FusedLocationApi.getLastLocation(gac);
+        Log.d("loc connected", "normally yes?" + loc);
+        if (loc != null) {
+           /* mLatitude.setText("Lat: " + loc.getLatitude());
+            mLongitude.setText("Lon: " + loc.getLongitude());*/
+
+            lati = loc.getLatitude();
+            longi = loc.getLongitude();
+
+        }
+    }
+
+
+
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    public void onConnectionSuspended(int connectionSuspended) {
+
     }
 
 }
