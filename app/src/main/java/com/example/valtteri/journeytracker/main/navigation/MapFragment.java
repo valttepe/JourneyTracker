ad92f3847f7802ed4f6ee44cc253b04e252f5374
odexi <otakar93@gmail.com>
2017-09-25 09:40:41 +0300

    Viikko 1 valmis

--- app/src/main/java/com/example/valtteri/journeytracker/main/navigation/MapFragment.java
+++ app/src/main/java/com/example/valtteri/journeytracker/main/navigation/MapFragment.java
@@ -25,6 +25,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.valtteri.journeytracker.BuildConfig;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.CommonStatusCodes;
