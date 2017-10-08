3910c190a96687dfe598f41327695debac5754aa
valttepe <valtteri.pesu@hotmail.com>
2017-10-08 19:23:17 +0300

    Some cleanups in code

--- app/src/main/java/com/example/valtteri/journeytracker/main/navigation/RouteFragment.java
+++ app/src/main/java/com/example/valtteri/journeytracker/main/navigation/RouteFragment.java
@@ -1,27 +1,18 @@
 package com.example.valtteri.journeytracker.main.navigation;
 
-import android.content.Context;
+
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.valtteri.journeytracker.R;
 import com.example.valtteri.journeytracker.route.tracking.RouteTrackActivity;
 
-/**
- * Created by Valtteri on 19.9.2017.
- */
 
 public class RouteFragment extends Fragment {
 
--- app/src/main/java/com/example/valtteri/journeytracker/route/tracking/OnFragmentInteractionListener.java
+++ app/src/main/java/com/example/valtteri/journeytracker/route/tracking/OnFragmentInteractionListener.java
@@ -2,14 +2,6 @@ package com.example.valtteri.journeytracker.route.tracking;
 
 import android.os.Bundle;
 
-import com.google.android.gms.maps.model.LatLng;
-
-import java.util.ArrayList;
-
-/**
- * Created by Valtteri on 21/09/2017.
- */
-
 public interface OnFragmentInteractionListener {
 
   void changeFragment(Bundle bundle);
